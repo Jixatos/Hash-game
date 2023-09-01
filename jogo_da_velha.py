c3e2430c130c047d01c598a4dc9fa8568778eef9
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-08-31 22:38:26 -0300

    finalizado

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,3 +1,5 @@
+import random
+
 def inicializarTabuleiro():
     while True:
         print("Jogar jogo da velha? Digite sim ou não")
@@ -15,9 +17,18 @@ def imprimirTabuleiro(tabuleiro):
         print("---------------")
 
 def imprimeMenuPrincipal():
-    print("Selecione o modo de jogo")
-    print("PvP - modoJogador")
-    print("PvE - modoFacil")
+    while True:
+        print("Selecione o modo de jogo")
+        print("1 - PvP - modoJogador")
+        print("2 - PvE - modoFacil")
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == '1':
+            modoJogador()
+        elif opcao == '2':
+            modoFacil()
+        else:
+            print("Opção inválida. Escolha novamente.")
 
 def leiaCoordenadaLinha():
     linha = input("Digite a linha que deseja jogar (1, 2 ou 3): ")
@@ -26,6 +37,16 @@ def leiaCoordenadaLinha():
 def leiaCoordenadaColuna():
     coluna = input("Digite a coluna que deseja jogar (1, 2 ou 3): ")
     return int(coluna) - 1
+
+def jogar(tabuleiro, jogador):
+    linha = leiaCoordenadaLinha()
+    coluna = leiaCoordenadaColuna()
+
+    if tabuleiro[linha][coluna] == '':
+        tabuleiro[linha][coluna] = jogador
+    else:
+        print("Posição inválida. Tente novamente.")
+        jogar(tabuleiro, jogador)
 
 def imprimePontuacao(player1, player2, maquinaF):
     print(f'A pontuação do Jogador 1 é de: {player1}')
@@ -51,48 +72,81 @@ def verificarVelha(tabuleiro):
     return all(all(linha != '' for linha in coluna) for coluna in tabuleiro)
 
 
-def jogoDaVelha():
+def modoJogador():
     tabuleiro = inicializarTabuleiro()
     player1 = 0
     player2 = 0
+    jogadas = 0
+    
+    while True:
+        jogador = 'X' if jogadas % 2 == 0 else 'O'
+        imprimirTabuleiro(tabuleiro)
+        jogadaUsuario(tabuleiro, jogador)
+        jogadas += 1
+        
+        if verificarVencedor(tabuleiro, jogador):
+            imprimirTabuleiro(tabuleiro)
+            print(f"Jogador {jogador} venceu!")
+            if jogador == 'X':
+                player1 += 1
+            else:
+                player2 += 1
+            imprimePontuacao(player1, player2, 0)
+            tabuleiro = inicializarTabuleiro()
+            jogadas = 0
+        elif verificarVelha(tabuleiro):
+            imprimirTabuleiro(tabuleiro)
+            print("Empate!")
+            tabuleiro = inicializarTabuleiro()
+            jogadas = 0
+
+def jogadaUsuario(tabuleiro, jogador):
+    print(f"Turno do Jogador {jogador}")
+    jogar(tabuleiro, jogador)
+    
+def modoFacil():
+    tabuleiro = inicializarTabuleiro()
+    player1 = 0
     maquinaF = 0
     jogadas = 0
     
-
     while True:
+        jogador = 'X' if jogadas % 2 == 0 else 'O'
         
-        jogador = 'X'
-        if jogadas % 2 == 0:
-            jogador = 'X'
+        if jogador == 'X':
+            imprimirTabuleiro(tabuleiro)
+            print(f"Turno do Jogador {jogador}")
+            jogar(tabuleiro, jogador)
         else:
-            jogador = 'O'
-            
-        imprimirTabuleiro(tabuleiro)
-        print(f"Turno do Jogador {jogador}")
-        
-        linha = leiaCoordenadaLinha()
-        coluna = leiaCoordenadaColuna()
-        
-        if posicaoValida(tabuleiro, linha, coluna):
+            linha, coluna = jogadaMaquinaFacil(tabuleiro)
             tabuleiro[linha][coluna] = jogador
-            jogadas += 1
+            print(f"A Máquina jogou na linha {linha + 1}, coluna {coluna + 1}")
             
-            if verificarVencedor(tabuleiro, jogador):
-                imprimirTabuleiro(tabuleiro)
-                print(f"Jogador {jogador} venceu!")
-                if jogador == 'X':
-                    player1 += 1
-                else:
-                    player2 += 1
-                imprimePontuacao(player1, player2, maquinaF)
-                tabuleiro = inicializarTabuleiro()  # Reinicia o jogo
-                jogadas = 0
-            elif verificarVelha(tabuleiro):
-                imprimirTabuleiro(tabuleiro)
-                print("Empate!")
-                tabuleiro = inicializarTabuleiro()  # Reinicia o jogo
-                jogadas = 0
-        else:
-            print("Posição inválida. Tente novamente.")
+        jogadas += 1
+        
+        if verificarVencedor(tabuleiro, jogador):
+            imprimirTabuleiro(tabuleiro)
+            if jogador == 'X':
+                print("Jogador venceu!")
+                player1 += 1
+            else:
+                print("Máquina venceu!")
+                maquinaF += 1
+            imprimePontuacao(player1, 0, maquinaF)
+            tabuleiro = inicializarTabuleiro()
+            jogadas = 0
+        elif verificarVelha(tabuleiro):
+            imprimirTabuleiro(tabuleiro)
+            print("Empate!")
+            tabuleiro = inicializarTabuleiro()
+            jogadas = 0
 
-jogoDaVelha()
+def jogadaMaquinaFacil(tabuleiro):
+    while True:
+        linha = random.randint(0, 2)
+        coluna = random.randint(0, 2)
+        if posicaoValida(tabuleiro, linha, coluna):
+            return linha, coluna
+        
+
+imprimeMenuPrincipal()
